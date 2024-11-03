b13befb26b5637f51061f5cf6118631758a04844
Dennis Göries <dennis.goeries@xfel.eu>
2024-11-03 20:16:04 +0100

    refactor: Align the triangle implementation

--- src/downsample/__init__.py
+++ src/downsample/__init__.py
@@ -1,8 +1,14 @@
 import numpy as np
 
 from downsample._ltd import largest_triangle_dynamic
+from downsample._ltob import largest_triangle_one_bucket
 
 
 def ltd(x: np.ndarray, y: np.ndarray, threshold: int):
     """Apply the largest triangle dynamic buckets algorithm"""
     return largest_triangle_dynamic(x, y, threshold)
+
+
+def ltob(x: np.ndarray, y: np.ndarray, threshold: int):
+    """Apply the largest triangle one buckets algorithm"""
+    return largest_triangle_one_bucket(x, y, threshold)
